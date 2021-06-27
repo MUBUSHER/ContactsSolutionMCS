038bf9cf4ad88e74269ab62a4b64dcc7beb9ba4f
mubusher@outlook.com <abcd1234>
2021-06-27 13:36:07 +0500

    Initial commit

---
+++ ContactsSolutionMCS/ContactDbContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactsSolutionMCS
+{
+    public class ContactDbContext : DbContext
+    {
+
+        public DbSet<ContactInfo> contactInfo { get; set; }
+
+        public ContactDbContext()
+            :base("ContactConnectionString")
+        { 
+        
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<ContactInfo>().ToTable("ContactInfo");
+        }
+    }
+}
+
+
---
+++ ContactsSolutionMCS/ContactInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactsSolutionMCS
+{
+    public class ContactInfo
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string LandlineNo { get; set; }
+        public string MobileNo { get; set; }
+        public string Email { get; set; }
+    }
+}
